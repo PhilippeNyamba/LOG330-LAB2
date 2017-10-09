9a0e921163330429b568c0e7a721e2bd733d8d28
PhilippeGervais <philippe-gervais.bang-nyamba.1@ens.etsmtl.ca>
2017-10-09 14:19:21 -0400

    Labo 2 testé

--- src/LecteurFichier.java
+++ src/LecteurFichier.java
@@ -1,41 +1,22 @@
-import javax.swing.*;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
- * Source: https://stackoverflow.com/questions/40074840/reading-a-csv-file-into-a-array
+ * @Auteur Philippe Nyamba
+ * @Date 2017-10-09
+ * Demarre l'application
  */
-public class LecteurFichier {
-
-    private List<List<String>> donneesLues;
-    File fichier;
-    Scanner inputStream;
-
-    public LecteurFichier(String chemin){
-        donneesLues = new ArrayList<>();
-        fichier = new File(chemin);
-    }
-
+public interface LecteurFichier {
     /**
-     * Remplit une liste à partir des donnees d'un fichier csv
+     * Effectue la lecture des données condignées dans un fichier et les stocke dans un attribut de classe
      * @throws FileNotFoundException
      */
-    public void lireFichier() throws FileNotFoundException {
-        {
-            inputStream = new Scanner(fichier);
-            while(inputStream.hasNext()){
-                String ligne = inputStream.next();
-                String[] valeurs = ligne.split(",");
-                donneesLues.add(Arrays.asList(valeurs));
-            }
-        }
-    }
+    public void lireFichier() throws FileNotFoundException;
 
-        public List<List<String>> getDonnees() {
-        return donneesLues;
-    }
+    /**
+     * Renvoie les données lues
+     * Doit être appelée apres lecture du fichier
+     * @return
+     */
+    public List<List<String>> getDonnees();
 }
